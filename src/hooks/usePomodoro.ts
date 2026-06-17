94cc0dfa8d7b663fb1b8d37ca17f6ea7090ed5b5
GennaroBuonomo <gennarobuonomo66@gmail.com>
2026-06-17 16:48:28 +0200

    feat: add timer interval logic to usePomodoro hook

--- src/hooks/usePomodoro.ts
+++ src/hooks/usePomodoro.ts
@@ -8,5 +8,16 @@ const SHORT_BREACK_TIME = 5 * 60;
 export const usePomodoro = () => {
   const [timeleft, setTimeLeft] = useState(FOCUS_TIMER);
   const [status, setStatus] = useState<TimerStatus>('idle');
-  const [mode, setMode] = useState<TimerMode>('focus')
+  const [mode, setMode] = useState<TimerMode>('focus');
+
+  useEffect(() => {
+    let intervall: number | undefined;
+
+    // Se il timer è in esecuzione e c'è ancora tempo, decurta 1 secondo
+    if (status === 'running' && timeLeft > 0) {
+      intervall = window.setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1)
+      }, 1000);
+    }
+  })
 }
